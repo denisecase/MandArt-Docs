42551b55ab49d9d6106e7b9957ec22d4d19d3c90
Denise Case <denisecase@gmail.com>
2024-04-11 12:36:41 -0500

    mandcolor + extra exps

--- redo.ps1
+++ redo.ps1
@@ -5,20 +5,6 @@
 # Define the root directory of the MandArt-Docs
 $rootDir = "."
 
-# Delete the old documentation in this root directory docs folder
-rm -rf docs
-
-Write-Host "Old documentation docs folder deleted."
-
-# Copy the new documentation to this root directory
-# Located at /Users/denisecase/Documents/MandArt/DerivedData/MandArt/Build/Products/Debug/MandArt.doccarchive
-
-$newDocs = "/Users/denisecase/Documents/MandArt/DerivedData/MandArt/Build/Products/Debug/MandArt.doccarchive"
-cp -r $newDocs .
-
-Write-Host $newDocs "copied to" $rootDir "Renaming to docs."
-
-
 # Rename MandArt.doccarchive to docs 
 rename-item MandArt.doccarchive docs
 
